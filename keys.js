ab8991d9ef082c1bf0fa4fed604bccf338a9b76a
Yasha Olatoto <yashanyou@gmail.com>
2017-01-25 15:54:44 -0600

    Delete keys.js

--- keys.js
+++
@@ -1,6 +0,0 @@
-exports.twitterKeys = {
-    consumer_key: 'dtO3rDEujMbAVFsUKJ0L8dGBL',
-    consumer_secret: 'DARBUnP0wIk4gvoH7hEaIZ8tuBlviHeB1DAQklUifZz5mChqXh',
-    access_token_key:'154442974-ifb3loAMw2v21DROd7w4Jj2nye8Pin3J822ULamN',
-    access_token_secret:'IFTHg3HRrvTG9DnUxAvXyEXo5hnQrirGuYaHdWLX5eix4'
-}
